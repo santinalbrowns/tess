416eab5e5371951fff9e2439c157b4d970b93ffc
Santinal Browns <asantikuseka@outlook.com>
2023-04-17 23:08:47 +0200

    remove web

--- web/src/hooks.server.ts
+++
@@ -1,7 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-
-export const handle: Handle = async ({ resolve, event }) => {
-  const response = await resolve(event);
-
-  return response;
-};
--- web/src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import "../app.css";
-    import 'tippy.js/dist/tippy.css';
-	import 'tippy.js/themes/light.css';
-    import 'material-symbols';
-</script>
-
-<slot/>
